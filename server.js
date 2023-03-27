dcffccc84793899a92aa0a3164145c50acd317fd
snhendri <sander.hendrickx@ugent.be>
2023-03-28 00:53:19 +0200

    basic test

--- server.js
+++ server.js
@@ -11,13 +11,27 @@ app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/img', express.static(__dirname + 'public/img'))
 app.use('/js', express.static(__dirname + 'public/js'))
 
+app.get('/data.json', (req, res) => {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error reading data.json');
+      return;
+    }
+
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.json(JSON.parse(data));
+  });
+});
 
 
 //app.use(bodyParser.urlencoded({ extended: true }))
 
 // Routes
-const trailRouter = require('./src/routes/trails')
-app.use('/', trailRouter)
+//const trailRouter = require('./src/routes/trails')
+//app.use('/', trailRouter)
 //app.use('/article', newsRouter)
 
 
